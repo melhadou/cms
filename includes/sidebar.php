83fcad48d1f39f80676a7c3344b98d983aef979c
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-09 12:16:32 +0100

    started a PHP CMS - project

---
+++ includes/sidebar.php
@@ -0,0 +1,72 @@
+<div class="col-md-4">
+
+    <!-- Blog Search Well -->
+
+    <div class="well">
+        <h4>Blog Search</h4>
+
+        <form action="search.php" method="POST">
+            <div class="input-group">
+                <input type="text" name="search" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="submit" name="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- /.input-group -->
+    </div>
+
+
+
+    <!-- Blog Categories Well -->
+    <?php
+
+$query = "SELECT * FROM categories";
+$select_sidbar_cat_querys = mysqli_query($connection, $query);
+
+?>
+    <div class="well">
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <?php
+
+while ($row = mysqli_fetch_assoc($select_sidbar_cat_querys)) {
+    $cat_title = $row['cat_title'];
+
+    echo "<li><a href='#'>{$cat_title}</a></li>";
+
+}
+
+?>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <div class="well">
+        <h4>Side Widget Well</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus
+            laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    </div>
+
+</div>
