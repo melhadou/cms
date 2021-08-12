0e7cbf837bbdd423661ec72dfe8131ccfd7e68b2
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-12 13:51:50 +0100

    Creating a HTML table in admin to display a list of posts

--- admin/includes/admin_nav.php
+++ admin/includes/admin_nav.php
@@ -42,7 +42,7 @@
                     Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#"> View All Posts</a>
+                        <a href="posts.php"> View All Posts</a>
                     </li>
                     <li>
                         <a href="#"> Add Posts</a>
