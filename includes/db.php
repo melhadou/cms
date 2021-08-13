06bac63f70f78810dead320c63c45ea39179b613
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-13 22:22:08 +0100

    changed db , to online db

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,5 @@
 <?php
-
-$connection = mysqli_connect('localhost', 'root', '', 'cms');
+// developement
+// $connection = mysqli_connect('localhost', 'root', '', 'cms');
+// online
+$connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
