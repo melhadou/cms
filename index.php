ce0ee906dfeae6a118337ee03449b5000aa77ab2
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-13 13:50:48 +0100

    Adding Individual Post Page and Link

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ $select_all_posts_querys = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_all_posts_querys)) {
     $post_title = $row['post_title'];
+    $post_id = $row['post_id'];
     $post_author = $row['post_author'];
     $post_date = $row['post_date'];
     $post_image = $row['post_image'];
@@ -37,21 +38,18 @@ while ($row = mysqli_fetch_assoc($select_all_posts_querys)) {
 
 
             <h2>
-                <a href="#"><?php echo $post_title ?></a>
+                <a href="posts.php?p_id=<?php echo $post_id ?>"><?php echo $post_title; ?></a>
             </h2>
             <p class="lead">
-                by <a href="index.php"><?php echo $post_author ?></a>
+                by <a href="index.php"><?php echo $post_author; ?></a>
             </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
             <hr>
             <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
             <hr>
-            <p><?php echo $post_content ?></p>
-            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-
-
-
+            <p><?php echo $post_content; ?></p>
+            <a class="btn btn-primary" href="posts.php?p_id=<?php echo $post_id ?>">Read More <span
+                    class="glyphicon glyphicon-chevron-right"></span></a>
             <?php }?>
 
 
