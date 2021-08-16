250c6f8f6a5a4b7cb0e9d13bf50a4cf78187af2c
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-16 22:20:01 +0100

    edit user feature

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,4 +1,26 @@
 <?php
+//pull data from db , => show it on , so we can edit it
+
+if (isset($_GET['edit_user'])) {
+
+    $u_id = $_GET['edit_user'];
+}
+
+$query = "SELECT * FROM users WHERE user_id = {$u_id}";
+$edit_user_query = mysqli_query($connection, $query);
+confirm($edit_user_query);
+while ($row = mysqli_fetch_assoc($edit_user_query)) {
+    $user_id = $row['user_id'];
+    $username = $row['username'];
+    $user_firstname = $row['user_firstname'];
+    $user_lastname = $row['user_lastname'];
+    $user_email = $row['user_email'];
+    $user_role = $row['user_role'];
+    $user_image = $row['user_image'];
+}
+?>
+<?php
+// edit the data , & send it back to db
 
 if (isset($_POST['edit_user'])) {
 
@@ -11,19 +33,34 @@ if (isset($_POST['edit_user'])) {
     $user_image = $_FILES['user_image']['name'];
     $user_image_temp = $_FILES['user_image']['tmp_name'];
 
-    //uploid image to images folder
+    //uploid image to user images folder
     move_uploaded_file($user_image_temp, "../users_images/$user_image");
 
-    $query = "UPDATE users SET username = '{$username}' ";
+    //check if there is no image
+    if (empty($user_image)) {
+
+        $query = "SELECT * FROM users WHERE user_id = {$u_id}";
+        $select_img = mysqli_query($connection, $query);
+
+        confirm($select_img);
+        while ($row = mysqli_fetch_assoc($select_img)) {
+            $user_image = $row['user_image'];
+        }
+
+    }
+
+    $query = "UPDATE users SET";
+    $query .= " username = '{$username}' ";
     $query .= ", user_password = '{$user_password}' ";
     $query .= ", user_firstname = '{$user_firstname}' ";
     $query .= ", user_lastname = '{$user_lastname}' ";
     $query .= ", user_email = '{$user_email}' ";
-    $query .= " ,user_role = '{$user_role}' ";
-    $query .= " ,user_image =  '{$user_image}'";
+    $query .= ", user_role = '{$user_role}' ";
+    $query .= ", user_image =  '{$user_image}'";
+    $query .= "WHERE user_id = {$u_id}";
 
-    $creat_user_query = mysqli_query($connection, $query);
-    confirm($creat_user_query);
+    $edit_user_query = mysqli_query($connection, $query);
+    confirm($edit_user_query);
 
 }
 
@@ -35,12 +72,11 @@ if (isset($_POST['edit_user'])) {
 
 
 
-
 <form action="" method="POST" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="username">Username</label>
-        <input type="text" class="form-control" name="username" id="username">
+        <input type="text" class="form-control" name="username" id="username" value="<?php echo $username; ?>">
     </div>
     <div class="form-group">
         <label for="user_password">Password</label>
@@ -48,16 +84,19 @@ if (isset($_POST['edit_user'])) {
 
     </div>
     <div class="form-group">
-        <label for="user_lastname">Last Name</label>
-        <input type="text" class="form-control" name="user_lastname" id="user_lastname">
+        <label for="user_firstname">First Name</label>
+        <input type="text" class="form-control" name="user_firstname" id="user_firstname"
+            value="<?php echo $user_firstname; ?>">
     </div>
     <div class="form-group">
-        <label for="user_firstname">First Name</label>
-        <input type="text" class="form-control" name="user_firstname" id="user_firstname">
+        <label for="user_lastname">Last Name</label>
+        <input type="text" class="form-control" name="user_lastname" id="user_lastname"
+            value="<?php echo $user_lastname; ?>">
     </div>
+
     <div class="form-group">
         <label for="user_email">Email</label>
-        <input type="email" class="form-control" name="user_email" id="user_email">
+        <input type="email" class="form-control" name="user_email" id="user_email" value="<?php echo $user_email; ?>">
     </div>
     <div class="form-group">
         <label for="user_role">Role</label>
@@ -67,7 +106,12 @@ if (isset($_POST['edit_user'])) {
         </select>
     </div>
     <div class="form-group">
-        <label for="image">User Image</label>
+        <label>Old User Image</label>
+
+        <img src="../users_images/<?php echo $user_image; ?>" class="img-responsive" width="50"
+            alt="<?php echo $user_image; ?>">
+        <br>
+        <label for="image">New User Image</label>
         <input type="file" class="form-control" name="user_image" id="image">
     </div>
 
--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -8,6 +8,7 @@
             <th>Last Name</th>
             <th>Email</th>
             <th>Role</th>
+            <th>Edit</th>
             <th>Delete</th>
             <th>Change Role</th>
 
@@ -38,6 +39,7 @@ while ($row = mysqli_fetch_assoc($select_user)) {
     echo "<td> $user_lastname</td>";
     echo "<td> $user_email</td>";
     echo "<td> $user_role</td>";
+    echo "<td><a href='users.php?source=edit_user&edit_user=$user_id'>Edit</a></td>";
     echo "<td><a href='users.php?delete=$user_id'>Delete</a></td>";
     echo "<td><a href='users.php?to_admin=$user_id'>Admin</a> <br>";
     echo "<a href='users.php?to_subs=$user_id'>Subscriber</a></td>";
