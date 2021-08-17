9d89cbcd838206396e6518f1bf19471f366d6ef9
mohamed elhadouchi <melhadouchi387@gmail.com>
2021-08-17 14:14:42 +0100

    select user query

--- includes/login.php
+++ includes/login.php
@@ -4,4 +4,16 @@
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-}
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
+
+    $select_username_query = mysqli_query($connection, $query);
+    if (!$select_username_query) {
+        die("QEURY FAILD " . mysqli_error($connection));
+    }
+    while ($row = mysqli_fetch_array($select_username_query)) {
+        echo $db_id = $row['user_firstname'];
+    }}
