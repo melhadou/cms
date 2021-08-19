3aa73a265c84b058d6348c33ca8d32f5ec4c627c
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-19 18:55:30 +0100

    add google chart to dashbord

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -45,6 +45,7 @@ if (!isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 </head>
 
