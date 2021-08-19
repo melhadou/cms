5491a60862be29647486a25a92852db7032a8378
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-19 13:32:31 +0100

    fix bug in db

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,5 @@
 <?php
 // developement
-<<<<<<< HEAD
-//$connection = mysqli_connect('localhost', 'root', '', 'cms');
-// online
-// $connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
-=======
 $connection = mysqli_connect('phpcms', 'admin', 'admin', 'cms');
 // online
 // $connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
->>>>>>> e5c21a3 (Changed OS to Linux)
