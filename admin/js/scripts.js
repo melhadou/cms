6cf3979c66f08a6452389f9097b8f69c1c4c7e22
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-21 00:56:10 +0100

    selecting all posts with js

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,3 +1,15 @@
 ClassicEditor.create(document.querySelector("#body")).catch((error) => {
   console.error(error);
 });
+
+$("#selectAllBoxes").click(function (event) {
+  if (this.checked) {
+    $(".checkBoxes").each(function () {
+      this.checked = true;
+    });
+  } else {
+    $(".checkBoxes").each(function () {
+      this.checked = false;
+    });
+  }
+});
