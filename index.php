8a0369d6cfdd037b6d5d36f48108fb76d0792d11
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-22 22:10:42 +0100

    show active page in pagination section

--- index.php
+++ index.php
@@ -93,7 +93,12 @@ while ($row = mysqli_fetch_assoc($select_all_posts_querys)) {
     <?php
 
 for ($i = 1; $i <= $count; $i++) {
-    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+    if ($i == $page) {
+        echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
+    } else {
+        echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+    }
+
 }
 
 ?>
