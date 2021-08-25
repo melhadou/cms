122243179dfa147b9e3f83fbf7ff0169abce43d9
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-25 14:01:01 +0100

    moved jquery to header

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,18 @@
 <!-- Footer -->
 <footer>
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; Mohamed Elhadouchi 2021</p>
-        </div>
-        <!-- /.col-lg-12 -->
+  <div class="row">
+    <div class="col-lg-12">
+      <p>Copyright &copy; Mohamed Elhadouchi 2021</p>
     </div>
-    <!-- /.row -->
+    <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
 </footer>
 
 </div>
 <!-- /.container -->
 
-<!-- jQuery -->
-<script src="js/jquery.js"></script>
+
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
