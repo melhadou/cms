cf5fb6d02ef1d820e546632214ad64a36f870351
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-28 01:39:17 +0100

    updating cat name with prepared stmt

--- admin/includes/update_categories.php
+++ admin/includes/update_categories.php
@@ -11,23 +11,28 @@ if (isset($_SESSION['user_role'])) {
     if (isset($_GET["edit"])) {
         $cat_id = escape($_GET["edit"]);
 
-        $query = "SELECT * FROM categories WHERE cat_id = {$cat_id} ";
-        $select_categories_id = mysqli_query($connection, $query);
-        while ($row = mysqli_fetch_assoc($select_categories_id)) {
-            $cat_title = escape($row['cat_title']);
-            $cat_id = escape($row['cat_id']);
-            ?>
+        $stmt = mysqli_prepare($connection, "SELECT cat_title,cat_id FROM categories WHERE cat_id = ?");
+        mysqli_stmt_bind_param($stmt, 'i', $cat_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $cat_title, $cat_id);
+        while (mysqli_stmt_fetch($stmt)):
+        ?>
     <input class="form-control" type="text" value="<?php if (isset($cat_title)) {echo $cat_title;}?>" name="cat_title"
       id="cat-title2">
 
-    <?php }
-    }?><?php
+    <?php
+endwhile;
+
+    }
+    ?><?php
 if (isset($_POST['update'])) {
         $the_cat_title = escape($_POST["cat_title"]);
 
-        $query = "UPDATE  categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id}";
+        //preparing stmt
+        $stmt = mysqli_prepare($connection, "UPDATE  categories SET cat_title = ? WHERE cat_id = ?");
 
-        $update_cat_title = mysqli_query($connection, $query);
+        mysqli_stmt_bind_param($stmt, 'si', $the_cat_title, $cat_id);
+        mysqli_stmt_execute($stmt);
     }
 
     ?>
