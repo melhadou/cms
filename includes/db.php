776f37a61763f9b4138a7e9914a69931fb698308
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-08-28 02:27:11 +0100

    change db to offline db

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
 // developement
-// $connection = mysqli_connect('phpcms', 'admin', 'admin', 'cms');
+ $connection = mysqli_connect('phpcms', 'admin', 'admin', 'cms');
 // online
-$connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
+// $connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
