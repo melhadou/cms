ba39fcdcbc993e5e7e3aa586f759589d3c14ee4c
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2021-10-07 21:47:37 +0100

    change to local db in arch

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 // developement
-$connection = mysqli_connect('localhost', 'root', '', 'cms');
+$connection = mysqli_connect('localhost', 'mohamed', 'mohamed', 'cms');
 // online
 // $connection = mysqli_connect('remotemysql.com', '9XA86yznmI', 'lTllpceGiy', '9XA86yznmI');
-?>
+
